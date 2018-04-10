cb77819863c8b60c9f0408954216b2b2528d6e03
Gerald Manipon <pymonger@gmail.com>
2018-04-10 00:34:49 +0000

    update unit tests for SciFlo-HySDS integration

--- sciflo/mapreduce/test.py
+++ sciflo/mapreduce/test.py
@@ -1,4 +1,4 @@
-import uuid, time
+import uuid, time, types, json, hashlib
 from datetime import timedelta
 from pprint import pprint
 from urllib import urlopen
@@ -33,7 +33,7 @@ def date_segmenter(startdate, enddate, segmentBy):
     return start_segments, end_segments, day_segments
 
 
-def test_map(startdate, enddate, segmentBy, arg1, arg2, wuid=None, job_num=None):
+def create_map_job(startdate, enddate, segmentBy, arg1, arg2, wuid=None, job_num=None):
     """Test function for map job json creation."""
 
     if wuid is None or job_num is None:
@@ -43,10 +43,12 @@ def test_map(startdate, enddate, segmentBy, arg1, arg2, wuid=None, job_num=None)
 
     return {
         "job_type": "job:parpython_map_job", 
+        "job_queue": "factotum-job_worker-small",
         "payload": {
             # sciflo tracking info
             "_sciflo_wuid": wuid,
             "_sciflo_job_num": job_num,
+            "_command": "/usr/bin/echo {} {} {} {}".format(start.year, start.month, arg1, arg2),
 
             # job params
             "year": start.year,
@@ -56,53 +58,61 @@ def test_map(startdate, enddate, segmentBy, arg1, arg2, wuid=None, job_num=None)
         }
     }
 
-def test_reduce(results):
-    """Test reduce function."""
 
-    reduced_result = []
-    for result in results:
-        job_url = result['job_url']
-        result = urlopen('%s/_stdout.txt' % job_url).read().strip()
-        reduced_result.append(result)
-    return '\n'.join(reduced_result)
-
-def test_reduce_async(task_ids):
-    """Test reduce function."""
-
-    res = GroupResult(id=uuid.uuid4(), results=[AsyncResult(id[0]) for id in task_ids])
-    while True:
-        ready = res.ready()
-        if ready: break
-        time.sleep(5)
-    return [i for i in res.join(timeout=10.)]
-
-def test_reduce_job(results, wuid=None, job_num=None):
+def create_reduce_job(results, wuid=None, job_num=None):
     """Test function for reduce job json creation."""
 
     if wuid is None or job_num is None:
         raise RuntimeError("Need to specify workunit id and job num.")
 
-    args = []
-    for result in results:
-        job_url = result['job_url']
-        result = urlopen('%s/_stdout.txt' % job_url).read().strip()
-        args.append("'%s'" % result)
-    
+    args = [ result['payload_id'] for result in results ]
     return {
         "job_type": "job:parpython_reduce_job", 
+        "job_queue": "factotum-job_worker-large",
         "payload": {
             # sciflo tracking info
             "_sciflo_wuid": wuid,
             "_sciflo_job_num": job_num,
+            "_command": "/usr/bin/echo {}".format(' '.join(args)),
 
             # job params
             "results": ' '.join(args),
         }
     }
 
-def get_reduce_result(result):
+
+def get_reduce_job_result(result):
     """Test function for processing a reduced job result."""
 
-    job_url = result['job_url']
-    job_res = urlopen('%s/_stdout.txt' % job_url).read().strip()
-    return job_res
+    print("got result: {}".format(json.dumps(result, indent=2)))
+    return result['payload_id']
+
+
+def join_map_jobs(task_ids):
+    """Test reduce function that manually joins all mapped jobs."""
+
+    print("task_ids: {}".format(json.dumps(task_ids, indent=2)))
+    res = GroupResult(id=uuid.uuid4(), results=[AsyncResult(id[0]) for id in task_ids])
+    while True:
+        ready = res.ready()
+        if ready: break
+        time.sleep(5)
+    results = []
+    for r in res.join(timeout=10.):
+        # deduped job?
+        if isinstance(r, (types.ListType, types.TupleType)):
+            # build resolvable result
+            task_id = r[0]
+            results.append({ 'uuid': task_id,
+                             'job_id': task_id,
+                             'payload_id': task_id,
+                             'status': 'job-deduped' })
+        else: results.append(r)
+    args = [ result['payload_id'] for result in results ]
+    return args
+
+
+def reduce_map_jobs(results):
+    """Test reduce function."""
+
+    return hashlib.md5(" ".join(results)).hexdigest()
